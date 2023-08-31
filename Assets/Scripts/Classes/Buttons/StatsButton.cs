9a799d786c5dbb3158976ee6c97fbaa2a80c7409
T3D0K <joesosspam@gmail.com>
2023-08-31 15:47:09 +0100

    StatScreen switching
    Added ability to swap between GoI stats and internal Foundation stats

---
+++ Assets/Scripts/Classes/Buttons/StatsButton.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class StatsButton : MonoBehaviour, IPointerClickHandler {
+
+    [Header("Buttons")]
+    [SerializeField] GameObject foundationStatButton;
+    [SerializeField] GameObject goiStatButton;
+
+    [Header("UI Panels")]
+    [SerializeField] GameObject foundationStatScreen;
+    [SerializeField] GameObject goiStatScreen;
+
+    //[Header("Events")]
+    //public GameEvent onStatScreenChanged;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        //TODO Check that the proposal can be accepted due to the stats
+        //This deals with the button used for accepting or denying the proposal
+        if(eventData.pointerPress == foundationStatButton) {
+            foundationStatScreen.SetActive(true);
+            goiStatScreen.SetActive(false);
+            
+        } else if (eventData.pointerPress == goiStatButton) {
+            goiStatScreen.SetActive(true);
+            foundationStatScreen.SetActive(false);
+        }
+        //StartCoroutine(AnimationCoroutine(canvasGroup));
+    }
+
+    // IEnumerator AnimationCoroutine(CanvasGroup canvasGroup)
+    // {
+    //     yield return new WaitForSeconds(3);
+    // }
+}
